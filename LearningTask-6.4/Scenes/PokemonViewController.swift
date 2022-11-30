edb384fed1cf2bddf8c01d2b405db2f42e47c5a9
Debora Rodrigues <debora.souza@zup.com.br>
2022-11-30 16:23:54 -0300

    Refatorar ViewController

--- LearningTask-6.4/Scenes/PokemonViewController.swift
+++ LearningTask-6.4/Scenes/PokemonViewController.swift
@@ -1,21 +1,36 @@
-//
-//  ViewController.swift
-//  LearningTask-6.4
-//
-//  Created by rafael.rollo on 12/04/2022.
-//
-
 import UIKit
+class PokemonViewController: UITableViewController {
 
-class PokemonViewController: UIViewController {
-
-    
-    @IBOutlet weak var tableView: UITableView!
+    var pokemonStore: PokemonStore?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupHeader()
+        
+    }
+    
+    func setupHeader() {
+        tableView.tableHeaderView = HeaderView.com(titulo: "Pokédex")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemonStore?.todos.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let pokemonCell = tableView.dequeueReusableCell(withIdentifier: "PokemonViewCell", for: indexPath) as? PokemonTableViewCell else {
+            fatalError("Error.")
+        }
+        
+        let pokemon = pokemonStore!.todos[indexPath.row]
+        pokemonCell.setup(pokemon)
+        
+        return pokemonCell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
