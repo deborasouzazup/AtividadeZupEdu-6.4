cb8deca4e84f0a75675237d4e959bb754eba4944
Debora Rodrigues <debora.souza@zup.com.br>
2022-11-30 15:49:49 -0300

    Reoeganizar main usando table view controller

---
+++ LearningTask-6.4/Scenes/Pokedex/View/PokemonViewCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonViewCellTableViewCell.swift
+//  LearningTask-6.4
+//
+//  Created by Debora.souza on 25/11/22.
+//
+
+import UIKit
+
+class PokemonViewCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
